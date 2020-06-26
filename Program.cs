1eb463f5bf7368c47597e49e753d595e5bc2dcfc
It <It@DESKTOP-VP6LV76>
2020-06-26 18:35:49 +0300

    Init commit

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.IO;
+using System.Net;
+using System.Net.Http;
+using System.Text;
+
+namespace SimbirSoft_Test
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello");
+            Program program = new Program();
+            program.Start();
+            Console.WriteLine("Press any key to Escape");
+            Console.ReadKey();
+        }
+
+        public void Start()
+        {
+            FileWorker fileWorker = new FileWorker();
+            HTMLWorker htmlWorker;
+            var urldata = fileWorker.ReadFile();
+            Console.WriteLine(urldata);
+            //htmlWorker = new HTMLWorker(urldata);
+            //using (WebClient client = new WebClient())
+            //{
+            //    string html = client.DownloadString("http://stackoverflow.com");
+            //    //Do something with html then
+            //    Console.WriteLine(html);
+            //}
+
+            //htmlWorker.SaveHTMLPages();
+
+            //HttpClient httpclient = new HttpClient();
+            //var result = httpclient.GetStringAsync(new Uri("https://www.microsoft.com/en-sg/"));
+            //File.WriteAllText("test.html", result);
+
+            var htmlDoc = new HtmlDocument();
+            htmlDoc.LoadHtml("https://www.microsoft.com");
+
+            FileStream sw = new FileStream("FileStream.html", FileMode.Create);
+
+            htmlDoc.Save(sw, Encoding.UTF8);
+        }
+    }
+}
